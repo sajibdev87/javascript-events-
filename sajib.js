587b76d6c628a6e4b4700dc049226cfaf579b092
sajibdev87 <145119503+sajibdev87@users.noreply.github.com>
2023-11-15 20:52:28 -0800

    y

---
+++ sajib.js
@@ -0,0 +1,41 @@
+// // local scope
+
+// var name ='sanjida';
+// function add(x,y){
+// var add =x+y;
+// console.log(add);
+// }
+// add(65,77)
+
+// // golbal Scope 
+
+// let yourName ='sajib';
+// function maName(){
+//           var myFullName ='sajib bhuiyan';
+//           console.log(myFullName);
+//           myFullName ='text';
+//           console.log(myFullName);
+//           console.log(yourName );
+//           yourName  ='rakib';
+//           return yourName   
+    
+// }
+// maName();
+// console.log(tyourName);
+
+// // primitive 
+
+// var parson ='sajib';
+// var hasFood ='orange';
+// console.log(parson);
+// hasFood='apple';
+// console.log(hasFood);
+
+
+// non primitive 
+
+let parson ={name:'sajib'}
+let newPreson=parson;
+parson.name='Shakib';
+console.log(parson);
+console.log(newPreson);
